05563a47daba4869536613709b0535e90f6730f4
vipa0z <abdelrahmanmagdy3660@gmail.com>
2025-12-26 06:24:28 +0200

    rotating but only on reload

--- themes/freemind.bithack/scripts/image_helper.js
+++ themes/freemind.bithack/scripts/image_helper.js
@@ -1,18 +1,29 @@
 var fs = require('fs');
 var path = require('path');
 
-hexo.extend.helper.register('list_images_in_dir', function (relativePath) {
-    var imgDir = path.join(process.cwd(), relativePath);
-    var images = [];
+
+hexo.extend.helper.register('get_random_image', function (subdir) {
+    var baseDir = hexo.source_dir || path.join(process.cwd(), 'source');
+    var imagesDir = path.join(baseDir, 'images', subdir);
+    var selectedImage = '';
+
     try {
-        if (fs.existsSync(imgDir)) {
-            images = fs.readdirSync(imgDir).filter(function (file) {
+        if (fs.existsSync(imagesDir)) {
+            var files = fs.readdirSync(imagesDir).filter(function (file) {
+                if (file.startsWith('.')) return false;
                 var ext = path.extname(file).toLowerCase();
-                return ['.jpg', '.jpeg', '.png', '.gif', '.avif', '.webp'].indexOf(ext) !== -1;
+                var validExts = ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg'];
+                return validExts.indexOf(ext) !== -1 && file !== 'cpts+back.png';
             });
+
+            if (files.length > 0) {
+                var randomIndex = Math.floor(Math.random() * files.length);
+                selectedImage = files[randomIndex];
+            }
         }
     } catch (e) {
-        console.error("Error listing images in " + imgDir + ": " + e);
+        console.error('[ImageHelper] Error getting image from ' + subdir + ':', e);
     }
-    return images;
+
+    return selectedImage;
 });
